18bb4335c50f0fe0411169f82b2e1c8d5bb7ce91
Dominic Leung <fatdom@gmail.com>
2017-01-23 17:44:51 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -76,8 +76,8 @@ osmbw = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 
 var stl = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com" target="_blank">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,  <a href="https://github.com/2blam/HK-geojson/" target="_blank">HK GeoJSON</a>, <a href="http://www.census2011.gov.hk/en/district-profiles/tai-po.html" target="_blank">2011 Population Census</a>',
-	minZoom: 16,
-	maxZoom: 11,
+	maxZoom: 16,
+	minZoom: 11,
 	ext: 'png'
 });
 
