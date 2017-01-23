3f7d5f455185769fce90d387a3635aeccdd01c58
Dominic Leung <fatdom@gmail.com>
2017-01-23 15:37:19 +0800

    Debug: onclick log coordinates

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,13 @@
 var map = new L.Map('map');
 
 var osmbw = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
+	maxZoom: 16,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-map.setView([22.451203, 114.169144], 8);
+map.setView([22.451203, 114.169144], 14);
 map.addLayer(osmbw);
+
+map.on('click', function(e){
+	console.log("Coordinate: " + e.latlng);
+});
