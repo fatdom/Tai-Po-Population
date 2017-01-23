70e18980f6eda65c02978d80e8801f11ca0dcb6f
Dominic Leung <fatdom@gmail.com>
2017-01-23 15:26:20 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([22.451203, 114.169144], 8);
+
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
