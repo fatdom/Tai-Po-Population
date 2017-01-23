29866f22b6264e1e11bd9e640444db04a8fc08e8
Dominic Leung <fatdom@gmail.com>
2017-01-23 15:32:25 +0800

    Add map layer and set view

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
-var map = L.map('map').setView([22.451203, 114.169144], 8);
+var map = new L.Map('map');
 
-var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+var osmbw = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
+
+map.setView([22.451203, 114.169144], 8);
+map.addLayer(osmbw);
